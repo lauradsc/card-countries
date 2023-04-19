283aff8d3022457d2bec34bd7930cde6e35077f6
Laura Costa <99484087+lauradsc@users.noreply.github.com>
2023-04-19 01:12:31 -0300

    Add files via upload

---
+++ card-countries/script.js
@@ -0,0 +1,35 @@
+$( "#japan" ).click(function() {
+    $( "#jp-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#france" ).click(function() {
+    $( "#fc-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#brazil" ).click(function() {
+    $( "#br-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#china" ).click(function() {
+    $( "#ch-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#spain" ).click(function() {
+    $( "#sp-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#greece" ).click(function() {
+    $( "#gc-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#southkorea" ).click(function() {
+    $( "#sk-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#switzerland" ).click(function() {
+    $( "#st-txt" ).slideToggle( "slow" );
+  });
+
+  $( "#unitedstates" ).click(function() {
+    $( "#us-txt" ).slideToggle( "slow" );
+  });
